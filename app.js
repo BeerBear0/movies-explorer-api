6473006207282be8a55aadbe6c59a462eb1cea6a
Nikita Chernov <ns4ern@gmail.com>
2021-07-29 18:07:13 +0300

    add all auth

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const express = require('express')
-const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const errorHandler = require('./middlewares/errorHandler');
 
+const app = express();
 const { PORT = 3000 } = process.env;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+mongoose.connect('mongodb://localhost:27017/diplomadb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+app.use('/', require('./routes/index'));
+
+app.use((req, res, next) => {
+    next(new Error('Not found'));
+});
+
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Сервер работает на ${3000} порту`)
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Joi, celebrate  } = require('celebrate');
+// const { urlValidator } = require('../helper/validator');
+const { login, createUser, logout } = require('../controllers/users');
+const usersRouter = require('./users');
+const moviesRouter = require('./movies');
+const auth = require('../middlewares/auth');
+
+router.post('/signin', celebrate({
+    body: Joi.object().keys({
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+    }),
+}), login);
+router.post('/signup', celebrate({
+    body: Joi.object().keys({
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+    }),
+}), createUser);
+
+router.use(auth);
+
+router.use('/users', usersRouter);
+router.use('/movies', moviesRouter);
+router.use('/*', (req, res, next) => {
+    const err = new Error('Not Found');
+    next(err);
+});
+router.get('/signout', logout);
+
+module.exports = router;
